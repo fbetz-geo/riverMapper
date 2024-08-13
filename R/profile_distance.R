2802362d9d029293e051ca79f0f05cd7ef8236bd
fbetz <riverandscience@gmail.com>
2024-08-13 12:05:30 +0200

    update function name

--- R/profile_distance.R
+++ R/profile_distance.R
@@ -2,13 +2,14 @@
 #' @description
 #' This function computes the distance along a channel line, where the line is "exploded" before computing distances. The function
 #' assumes the channel line being in projected coordinates and will give an error if this is not the case. 
-#'@param x sf object containing the channel line
+#'@param riverline sf object containing the channel line
 #'@param maxDist maximum distance (in meter) between two consecutive vertices when densifying the vertices of the line. 
 #'Default is 10 m.In general, maxDist should be in a similar range as the resolution of the underlying elevation data. 
 #'@author Florian Betz
+#'@return an sf points object containing the downstream distances along the channel line along with their downstream distance
 #'@export profile_distance
 #'
-profile_distance<-function(x, maxDist=10){
+profile_distance<-function(riverline, maxDist=10){
 
   #Check for projected coordinates
   if (sf::st_is_longlat(x)) {
@@ -19,12 +20,9 @@ profile_distance<-function(x, maxDist=10){
   #Densify line vertices
   dist_line<-sf::st_segmentize(x,dfMaxLength = units::set_units(maxDist,m))
   
-  #Convert to points as basis for distance calculation
-  dist_pts<-sf::st_cast(dist_line,"POINT")
-  
-  #Compute distances
-  dists<-sf::st_coordinates(dist_pts) %>% as.data.frame() %>% 
-    dplyr::mutate(dists=cumsum(c(0,sqrt(diff(X)^2 + diff(Y)^2))))
+  #Extract coordinates and compute distances
+  dists<-sf::st_coordinates(dist_line) %>% as.data.frame() %>% 
+    dplyr::mutate(distances=cumsum(c(0,sqrt(diff(X)^2 + diff(Y)^2))))
   
   return(dists)
   
