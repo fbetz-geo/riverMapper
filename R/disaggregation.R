03fe5a2af1f70ca92742f3ed8ad237eed8877370
fbetz <riverandscience@gmail.com>
2024-08-16 21:37:54 +0200

    add correct ordering by distance and elevation

--- R/disaggregation.R
+++ R/disaggregation.R
@@ -6,26 +6,55 @@
 #' alternatively, mask can also be a sf object with a polygon representing the river corridor 
 #' @param seg_length length of the desired river segments; choosing the appropriate scale of analysis will depend very much 
 #' on the resolution of the input data and the scope of the specific analysis; default is 1 km
+#' @param start where should the profile distance start? "Highest" will start compute the distances from source to mouth, 
+#'"lowest" from mouth to source
+#' @param dem path to a digital elevation model stored in any common raster data format, this is required for determining start and end by elevation
 #' @author Florian Betz
 #' @return a list with 1) the centerline as sf line object and 2) the smoothed polygon as sf object
 #' @export disaggregation
 #' 
 
-disaggregation<-function(c_line,mask,seg_length=1000){
+disaggregation<-function(c_line,mask,seg_length=1000,start="lowest",dem){
   
   #Create polygon from the mask
   if (is(mask,"SpatRaster")) {
-    poly<-terra::as.polygons(mask)
+    poly<-terra::as.polygons(mask) %>% terra::fillHoles()
   }else{
-    poly<-vect(mask)
+    poly<-vect(mask) %>% terra::fillHoles()
   }
   
+  #Temporary store coordinate system
+  c_rs<-sf::st_crs(c_line)
+  
+  #Check if the centerline is only one feature
+  if (nrow(c_line)>1) {
+    c_line<-sf::st_union(c_line)
+    
+  }
+
+  #Convert centerline to sf LINESTRING if necessary
+  if(sf::st_geometry_type(c_line)=="MULTILINESTRING"){
+    c_line<-sf::st_line_merge(c_line)
+  }
+    
   #Create points in the specified distance from the centerline and convert to terra::vect() format to avoid errors with sf::st_voronoi
-  pts<-sf::st_segmentize(c_line,dfMaxLength = seg_length) %>% sf::st_coordinates() %>% as.data.frame() %>% 
-    sf::st_as_sf(coords=c("X","Y"),crs=crs(riverline)) %>% terra::vect()
+  pts<-sf::st_line_sample(c_line, density = 1/seg_length,type = "regular") %>% terra::vect()
   
   #Create Voronoi polygons from the points
-  segs<-terra::voronoi(pts,bnd=poly) %>% sf::st_as_sf() %>% dplyr::mutate(seg_id=dplyr::row_number())
+  segs_init<-terra::voronoi(pts,bnd=poly) %>% terra::intersect(poly)  %>% sf::st_as_sf() %>% 
+    dplyr::mutate(seg_id=dplyr::row_number())
+  
+  #Compute distances along centerline
+  c_line_dist<-riverMapper::profile_distance(riverline=c_line,maxDist = seg_length/2,start=start,dem=dem)
+  
+  
+  #Make spatial join of distance object and segments
+  segs_with_lengths<-sf::st_join(segs_init,c_line_dist) %>% 
+    dplyr::group_by(seg_id) %>% 
+    dplyr::summarize(centerline_distance=median(distances)) %>% 
+    dplyr::arrange(centerline_distance)
+  
+  segs<-segs_with_lengths %>% dplyr::select(centerline_distance) %>% dplyr::mutate(segment_id=dplyr::row_number())
   
   return(segs)
 } 
--- R/profile_distance.R
+++ R/profile_distance.R
@@ -3,26 +3,57 @@
 #' This function computes the distance along a channel line, where the line is "exploded" before computing distances. The function
 #' assumes the channel line being in projected coordinates and will give an error if this is not the case. 
 #'@param riverline sf object containing the channel line
+#'@param start where should the profile distance start? "Highest" will start compute the distances from source to mouth, 
+#'"lowest" from mouth to source
+#'@param dem path to a digital elevation model stored in any common raster data format, this is required for determining start and end by elevation
 #'@param maxDist maximum distance (in meter) between two consecutive vertices when densifying the vertices of the line. 
 #'Default is 10 m.In general, maxDist should be in a similar range as the resolution of the underlying elevation data. 
 #'@author Florian Betz
 #'@return an sf points object containing the downstream distances along the channel line along with their downstream distance
 #'@export profile_distance
 #'
-profile_distance<-function(riverline, maxDist=10){
+profile_distance<-function(riverline, start="highest", dem,maxDist=10){
 
   #Check for projected coordinates
-  if (sf::st_is_longlat(x)) {
+  if (sf::st_is_longlat(riverline)) {
     stop("profile_distance expects a projected coordinate system for computing distances. Please consider reprojecting")
   
   }
-    
+  
+  #Temporary store coordinate system including check if coordinate system object exists already
+  if (!exists("c_rs")) {
+    c_rs<-sf::st_crs(riverline)
+  }
+  
   #Densify line vertices
-  dist_line<-sf::st_segmentize(x,dfMaxLength = units::set_units(maxDist,m))
+  dist_line<-sf::st_segmentize(riverline,dfMaxLength = units::set_units(maxDist,m))
   
   #Extract coordinates and compute distances
-  dists<-sf::st_coordinates(dist_line) %>% as.data.frame() %>% 
-    dplyr::mutate(distances=cumsum(c(0,sqrt(diff(X)^2 + diff(Y)^2))))
+  elev_pts<-sf::st_coordinates(dist_line) %>% as.data.frame() %>% 
+    
+    #Make points to extract values from DEM
+    sf::st_as_sf(coords=c("X","Y"),crs=c_rs)
+  
+  #Grab elevation values from DEM and sort data.frame
+  dem<-rast(dem)
+  elev_pts$elev<-NA
+  elev_pts$elev[1]<-terra::extract(dem, terra::vect(elev_pts[1,]))[1,2]
+  elev_pts$elev[nrow(elev_pts)]<-terra::extract(dem, terra::vect(elev_pts[nrow(elev_pts),]))[1,2]
+  elev_pts$elev<-stats::approx(x = 1:nrow(elev_pts),y=elev_pts$elev,xout=1:nrow(elev_pts))$y
+  
+  #For highest to lowest, i.e. source to mouth
+  if (start=="highest") {
+    elev_pts<-elev_pts %>% dplyr::arrange(dplyr::desc(elev))
+  }
+  
+  #For lowest to highest, i.e. mouth to source
+  if (start=="lowest") {
+    elev_ots<-elev_pts %>% dplyr::arrange(elev)
+    
+  }
+  dists<-sf::st_coordinates(elev_pts) %>% as.data.frame() %>% 
+    dplyr::mutate(distances=cumsum(c(0,sqrt(diff(X)^2 + diff(Y)^2)))) %>% 
+    sf::st_as_sf(coords=c("X","Y"),crs=c_rs)
   
   return(dists)
   
